ec5deabe35ed658dff7a8e90abd92170e41d1fbb
nboober <nboober@terpmail.umd.edu>
2019-07-16 12:02:55 -0400

    Added a second actor and movie for that actor

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -24,6 +24,11 @@ public class HomeController {
         actor.setName("Sandra Bullock");
         actor.setRealname("Sandra Mae Bullowski");
 
+        //First let's create an actor
+        Actor actor2 = new Actor();
+        actor2.setName("Tom Hanks");
+        actor2.setRealname("Timothy Hankanson");
+
         //Now let's create a movie
         Movie movie = new Movie();
         movie.setTitle("Emoji Movie");
@@ -34,18 +39,34 @@ public class HomeController {
         Movie movie2 = new Movie();
         movie2.setTitle("Pretty Woman");
         movie2.setYear(1990);
-        movie.setDescription("About a Pretty Women...");
+        movie2.setDescription("About a Pretty Women...");
 
-        //Add the movies to an empty list
+        //Add a third Movie
+        Movie movie3 = new Movie();
+        movie3.setTitle("Toy Story");
+        movie3.setYear(1990);
+        movie3.setDescription("About a Toy's Story...");
+
+        //Add the movies for Sandra Bullock to an empty list
         Set<Movie> movies = new HashSet<Movie>();
         movies.add(movie);
         movies.add(movie2);
 
-        //Add the list of movies to the actor's movie list
+        //Add movies for Tom Hanks to another empty list
+        Set<Movie> movies2 = new HashSet<Movie>();
+        movies2.add(movie3);
+
+        //Add the list of movies to the Sandra Bullock's movie list
         actor.setMovies(movies);
+
+        //Add the list of movies to the Tom Hanks movie list
+        actor2.setMovies(movies2);
 
         //Save the actor to the database
         actorRepository.save(actor);
+
+        //Save the actor to the database
+        actorRepository.save(actor2);
 
         //Grab all the actors from the database and send them to the template
         model.addAttribute("actors", actorRepository.findAll());
